9ce8d2f5e153bc010a04fed8f3cd2217c0d8d97c
rstonon <rstonon@gmail.com>
2021-04-14 18:47:27 -0300

    Api Orders Auth

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ Route::group([
 ], function() {
     Route::get('/auth/me', [AuthClientController::class, 'me']);
     Route::post('/auth/logout', [AuthClientController::class, 'logout']);
+
+    Route::get('/auth/v1/myorders', [OrderApiController::class, 'myOrders']);
+    Route::post('/auth/v1/orders', [OrderApiController::class, 'store']);
 });
 
 
